ce7e6ee5b99cb889b5e5798983fcb08d4294af5a
Lee <lee.jensen@agilix.com>
2024-02-26 14:21:47 -0700

    join in working order with plater name

--- startup_javascript/join.js
+++ startup_javascript/join.js
@@ -1,8 +1,14 @@
-function buildPage() {
+function AddPlayerName() {
+   console.log("starting to set player name...")
    const playerNameElement = document.querySelector('#player-name');
-   playerNameElement.textContent = ("Player: "+ getPlayerName());
-   console.log("set player name")
+   let usernameString = getPlayerName();
+   playerNameElement.textContent = usernameString;
+   console.log("set player name");
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+   AddPlayerName();
+})
 
 function getPlayerName() {
    return localStorage.getItem('userName');
