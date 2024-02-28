7c9bffae0e0616dacaa807496caf76f59ddaa9a4
Lee <lee.jensen@agilix.com>
2024-02-27 22:48:13 -0700

    got the countdown & round numbers in fairly working order

--- startup_javascript/play.js
+++ startup_javascript/play.js
@@ -23,15 +23,42 @@ const SEC_PER_ROUND = 3;
 const NEXT_ROUND_LUCK = 0.16;
 const DOUBLES_LUCK = 0.33;
 
-localStorage.setItem('ROUND_NUMBER', 0);
+let round = 0;
 let secondsLeft = SEC_PER_ROUND;
 const roundNumberElement = document.querySelector('#round-label')
 const secLeftElement = document.querySelector('.countdown-timer')
 
+
+
 // Play a certain number of rounds
-for(let i = 1; i <= NUMBER_OF_ROUNDS; i++) {
-   roundNumberElement.textContent = "ROUND " + i + " of " + NUMBER_OF_ROUNDS;
-   secLeftElement.textContent = secondsLeft;
-   // Update the countdown timer every second until it equals 0
+playRounds();
+
+async function playRounds(){
+   round++;
+   if(round<NUMBER_OF_ROUNDS){
+      roundNumberElement.textContent = "Round " + round + " of " + NUMBER_OF_ROUNDS;
+      secondsLeft = SEC_PER_ROUND;
+      secLeftElement.textContent = secondsLeft;
+   }
+   console.log("round " + round);
+   try{await countDown();}
+   catch{console.log('countdown failed.')}
+   finally{
+      print('playrounds finally')
+   }
 }
 
+async function countDown() {
+   return new Promise((resolve,reject) => {
+      if(secondsLeft>0){
+         setTimeout( () => {
+            secondsLeft--;
+            secLeftElement.textContent = secondsLeft;
+            countDown();
+         }, 1000);
+      }
+      else{
+         console.log("round " + round + " ended.")
+      }
+   });
+}
