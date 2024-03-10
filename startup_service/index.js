b6b1bbd1d0d185b83472836335c0e45bf5cf0737
Lee <lee.jensen@agilix.com>
2024-03-09 18:52:41 -0700

    initial commit for backend score handling service

--- startup_service/index.js
+++ startup_service/index.js
@@ -21,4 +21,48 @@ app.use((_req, res) => {
 
 // I need a service to store all the high scores.
 // Save to server memory and then reset only when the service is reset
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// process a request that submits a new score
+apiRouter.post('/score', (req, res) => {
+   console.log('in apiRouter.post...');
+   // take the information from the request and save it: save the score
+   scoreArray = updateScores(req.body, scoreArray);
+   res.send(scoreArray);
+   console.log('score sent to top score list.');
+});
+
+// Send the all time best scores back:
+apiRouter.get('/scores', (_req, res) => {
+   res.send(scoreArray);
+   console.log('Score array sent back to client');
+});
+
 let scoreArray = [];
+function updateScores(newScore, scores) {
+   let areThereScores = false;
+   for (const [i, prevScore] of scoreArray.entries()) {
+      if (newScore.score > prevScore.score) {
+         // The score is better than the one at this point in the list
+         // -> insert the score into the array
+         scores.splice(i, 0, newScore);
+         areThereScores = true;
+         break;
+      }
+   }
+
+   // If the score wasn't better than any on the list, stick it at the end
+   // -> Also covers the case where there are no high scores yet.
+   if (!areThereScores){
+      scores.push(newScore);
+   }
+
+   // Sample code includes this so the high score data doesn't save an
+   //    infinite number of high scores. Keep only top 10.
+   if (scores.length > 10) {
+      scores.length = 10;
+   }
+
+   return scores;
+}
