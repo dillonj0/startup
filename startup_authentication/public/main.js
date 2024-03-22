9fa62013e8457790df3eda7dcdcd8154310304c9
Lee <lee.jensen@agilix.com>
2024-03-22 11:06:08 -0600

    adapted login code to reference backend

--- startup_authentication/public/main.js
+++ startup_authentication/public/main.js
@@ -1,17 +1,59 @@
-function login() {
-   const usernameElement = document.querySelector('#username');
+(async () => {
+   // simon code uses a function like this to display the username
+   //    if there's a previously stored value, rather than making the
+   //    user log back in every time they go to the home page
+   const username = localStorage.getItem('userName');
+   if (username) {
+      console.log('user is already logged in as' + username);
 
-   localStorage.setItem('userName', usernameElement.value);
-   console.log(localStorage.getItem('userName'));
+      // TODO: fix this so the user doesn't have to log in again
 
-   if(document.querySelector('#password').value !== '') {
-      console.log("redirecting to join.html");
-      window.location.href='join.html';
-      
+   }
+})();
+
+function usernameAndPasswordCheck(){
+   if(document.querySelector('#userPassword').value !== '' &&
+      document.querySelector('#userName').value !== '') {
+      return true;
    }
    else {
-      console.log('User did not provide password.');
-      alert("You must provide a username and password to play.");
+      console.log('User did not provide username and/or password.');
+      alert("You must provide a username and password.");
+      return false;
+   }
+}
+
+async function login() {
+   if(usernameAndPasswordCheck()){
+      loginOrCreate(`/api/auth/login`)
+   }
+}
+
+async function createUser() {
+   if(usernameAndPasswordCheck()){
+      loginOrCreate(`/api/auth/createUser`)
+   }
+}
+
+async function loginOrCreate(endpoint) {
+   // send the username and password to the server for verification
+   const username = document.querySelector('#userName')?.value;
+   const password = document.querySelector('#userPassword')?.value;
+   const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+      },
+   });
+
+   if (response.ok) {
+      localStorage.setItem('userName', username);
+      console.log('logged in as' + username);
+      window.location.href='join.html';
+   } else {
+      const body = await response.json();
+      
    }
 }
 
