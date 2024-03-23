35f5c79400b9524ec51b8d39715260787be38d0b
Lee <lee.jensen@agilix.com>
2024-03-22 23:07:18 -0600

    introducing more logical database things

--- startup_authentication/index.js
+++ startup_authentication/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const DB = require('./database.js');
+const bcrypt = require('bcrypt');
+const cookieParser  =require('cookie-parser');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -7,10 +10,50 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.json());
 app.use(express.static('public', {root: __dirname}));
 
+// Use cookie parser middleware for parsing tokens for authentication
+app.use(cookieParser());
+
 // I need a service to store all the high scores.
 // Save to server memory and then reset only when the service is reset
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+// user asks to create a new user
+apiRouter.post('/auth/create', async (req, res) => {
+   if (await DB.getUser(req.body.username)) {
+      res.status(409).send({ msg: 'Tried to create a new account for an existing user :('});
+   } else {
+      // This username is not on record: create a new user
+      const user = await DB.createUser(req.body.email, req.body.password);
+
+      // set the cookie
+      setAuthCookie(res, user.token);
+
+      res.send({
+         id: user._id,
+      });
+   }
+});
+
+// When I incorporate logout functionality, need a way to delete the token
+apiRouter.delete('/auth/logout', (_req, res) => {
+   res.clearCookie(authCookieName);
+   res.status(204).end();
+});
+
+// user calls login function: authorize or not
+apiRouter.post('/auth/login', async (req, res) => {
+   const user = await DB.getUser(req.body.username);
+   if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+         setAuthCookie(res, user.token);
+         res.send({id: user.id});
+         return;
+      }
+   } else {
+      res.status(401).send({ msg: 'Unauthorized!'});
+   }
+});
 
 // process a request that submits a new score
 apiRouter.post('/score', (req, res) => {
@@ -33,6 +76,15 @@ app.use((_req, res) => {
    res.sendFile('index.html', {root: 'public'});
    console.log('unknown page requested, redirecting client to home page...');
 });
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+   res.cookie(authCookieName, authToken, {
+     secure: true,
+     httpOnly: true,
+     sameSite: 'strict',
+   });
+}
 
 // Listen on port 4000 or a port specified when you run the file
 app.listen(port, () => {
